9f0ca0e40e851c755c5984f43689e289b64343a2
Ken Jiang <kenzoengineer@gmail.com>
2021-10-28 15:34:18 +0000

    Made insert distinct and added delete

--- index.js
+++ index.js
@@ -1,58 +1,82 @@
 const Discord = require('discord.js');
 const Moment = require('moment');
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 const trackerDb = "TextTracker";
 const wordsCollection = "TrackedWords";
-
 const client = new Discord.Client();
 const uri = `mongodb+srv://replit-user-01:${process.env['password']}@mybotcluster.mbqon.mongodb.net/TextTracker?retryWrites=true&w=majority`;
 const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.on('ready', async () => {
     client.user.setActivity('!help');
+    await mongoClient.connect();
     await console.log('ready!');
 });
 
-const dbInsert = async (_guild,_word,_user) => {
-    await mongoClient.connect();
-    const col = mongoClient.db(trackerDb).collection(wordsCollection);
-    await col.insertOne({
-        guild_id: _guild,
-        word: _word,
-        user_id: _user
-    });
-    mongoClient.close();
+const getErrorEmbed = (desc) => {
+    return new Discord.MessageEmbed()
+        .setColor('#E85959')
+        .setTitle('Error!')
+        .setDescription(desc);
 }
 
-const dbGet = async (_guild) => {
-    await mongoClient.connect();
+const dbInsert = async (_guild, _word, _user) => {
+    let findResults = await dbGet(_guild, _word, _user);
+    if (findResults.length > 0) {
+        return false;
+    }
+    const col = mongoClient.db(trackerDb).collection(wordsCollection);
+    await col.insertOne({guild_id: _guild,word: _word,user_id: _user});
+    return true;
+}
+
+const dbGet = async (_guild, _word, _user) => {
+    const col = mongoClient.db(trackerDb).collection(wordsCollection);
+    let res = await col.find({guild_id: _guild, word: _word, user_id: _user}).toArray();
+    return res;
+}
+
+const dbGetAll = async (_guild) => {
     const col = mongoClient.db(trackerDb).collection(wordsCollection);
     let res = await col.find({guild_id: _guild}).toArray();
-    mongoClient.close();
     return res;
+}
+
+const dbDelete = async (_guild, _word, _user) => {
+    const col = mongoClient.db(trackerDb).collection(wordsCollection);
+    await col.deleteOne({guild_id: _guild, word: _word, user_id: _user});
 }
 
 client.on('message', async (msg) => {
     if (msg.content.startsWith('!')) { //prefix
         let fullCmd = msg.content.split(' ');
         let cmd = fullCmd[0].substring(1);
+
         if (cmd === "add") {
             if (fullCmd.length < 3) {
-                await msg.channel.send("Not enough parameters.");
-            } else {
-                let user = fullCmd[1].replace("<@!","").replace(">","");
-                let word = fullCmd[2];
-                try {
-                    await dbInsert(msg.guild.id,word,user);
+                await msg.channel.send({
+                    embed: getErrorEmbed('Not enough arguments. Please use: `!add <user> <word>`')
+                });
+                return;
+            }
+            let user = fullCmd[1].replace("<@!","").replace(">","");
+            let word = fullCmd[2];
+            try {
+                let result = await dbInsert(msg.guild.id,word,user);
+                if (result) {
                     await msg.channel.send("Success.");
-                } catch (e) {
-                    console.log(e);
+                } else {
+                    await msg.channel.send("Entry already exists.");
                 }
+                
+            } catch (e) {
+                console.log(e);
             }
         }
+
         if (cmd === "get") {
-            let res = await dbGet(msg.guild.id);
+            let res = await dbGetAll(msg.guild.id);
             let words = res.map(x => x.word).join("\n");
             let users = res.map(x => client.users.cache.find(y => y.id = x.user_id)).join("\n");
             const embed = new Discord.MessageEmbed()
@@ -64,7 +88,33 @@ client.on('message', async (msg) => {
                 );
             msg.channel.send({embed});
         }
+        
+        if (cmd === "remove") {
+            if (fullCmd.length < 3) {
+                await msg.channel.send({
+                    embed: getErrorEmbed('Not enough arguments. Please use: `!remove <user> <word>`')
+                });
+                return;
+            }
+            let user = fullCmd[1].replace("<@!","").replace(">","");
+            let word = fullCmd[2];
+            try {
+                await dbDelete(msg.guild.id, word, user);
+                await msg.channel.send("Success.");
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        
+        if (cmd === "close") {
+            mongoClient.close();
+        }
     }
+});
+
+process.on('exit', () => {
+    mongoClient.close();
+    console.log('exiting...');
 });
 
 client.login(process.env['token']);
